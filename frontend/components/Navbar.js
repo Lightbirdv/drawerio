4e9541884a75e3a53bd071382d31c44373ac8d81
Timothy <t.gee@hotmail.de>
2022-06-22 17:53:52 +0200

    git test again

--- frontend/components/Navbar.js
+++ frontend/components/Navbar.js
@@ -33,9 +33,9 @@ export default function Navbar() {
     return (
         <nav className="flex filter drop-shadow-md bg-white px-4 py-4 h-20 items-center">
             <MobileNav open={open} setOpen={setOpen}/>
-            <div className="w-3/12 flex items-center">
+            <div className="w-3/12 flex items-center ml-28">
                 <a className="text-2xl font-semibold" href="/"><img src="assets/logo.jpg" className="img-logo" style={{marginRight: "15px", maxHeight: "130px"}}/></a>
-                <a className="text-2xl font-semibold" href="/">Drawer.io</a>
+                <a className="text-2xl font-semibold text-blue-800 hover:no-underline" href="/">Drawer.io</a>
             </div>
 
             <div className="w-9/12 flex justify-end items-center">
@@ -49,12 +49,12 @@ export default function Navbar() {
                     <span className={`h-1 w-full bg-black rounded-lg transform transition duration-300 ease-in-out ${open ? "-rotate-45 -translate-y-3.5" : ""}`} />
                 </div>
 
-                <div className="hidden md:flex">
+                <div className="hidden md:flex items-center mr-20">
                     <NavLink to="/login">
-                    <button className="x">Login</button>
+                    <button className="bg-white w-36 h-10 border-2 border-blue-800 text-blue-800 rounded text-xl">Login</button>
                     </NavLink>
                     <NavLink to="/registration">
-                    <button className="y">Sign Up</button>
+                    <button className="bg-blue-800 w-36 h-10 text-white rounded text-xl">Sign Up</button>
                     </NavLink>
                 </div>
             </div>
--- frontend/components/TopComponent.js
+++ frontend/components/TopComponent.js
@@ -1,50 +1,43 @@
-import React from 'react'
+import React from "react";
 import { Col, Container, Row } from "react-bootstrap";
-import Router from 'next/router';
-import 'bootstrap/dist/css/bootstrap.css';
-
+import Router from "next/router";
+import "bootstrap/dist/css/bootstrap.css";
 
 function TopComponent() {
-
   const handleClick = (e, path) => {
-    if(path == "/reg"){
-    Router.push("/registration")
-     }
+    if (path == "/reg") {
+      Router.push("/registration");
+    }
   };
 
   return (
-    /* fragment statt div, beides ok */
     <>
-      <div className="background"></div>
-
-      <div className="container" style={{ marginTop: '10px' }}>
-        <div className="row">
-          <div className="col-sm-4" id="xxx">
-            <div className="fakeimg">
-              <div className="center"><h1 style={{fontSize:"35px", marginTop:"5px"}}>Drawer</h1>
-                <h2 style={{fontSize:"25px", marginTop:"5px"}}>What is Drawer.io?</h2>
-                <p style={{fontSize:"15px", marginTop:"5px"}}>
-                  Drawer.io is a chrome extension with which you can put together your own virtual drawers with all sorts of videos, images and texts that interest you.
-                </p>
-                <div margin-top="10px"></div>
-                <p style={{fontSize:"15px", marginTop:"5px"}}>Expand your browser now with our great application</p>
-                <p style={{fontSize:"15px", marginTop:"5px"}}>Get Your own Drawer and have an organized future</p>
-                <p style={{fontSize:"25px", marginTop:"10px"}}>
-                  Sign up and get your saved Datas here!
-                </p>
-                <div class="w3-container">
-                {/* <input type="text" id="first" name="first" placeholder="Email" style={{ marginTop: "15px", padding: "3px", borderRadius: "15px" }} /> */}
-                <button className="z" onClick={(e) => handleClick(e, "/reg")}>Sign Up</button></div>
-                </div>
-            </div>
+      <div className="bg-emerald-200">
+        <div className="TopComponent-Container flex flex-row w-4/5 m-auto items-center ">
+          <div className="TopComponent-Cta w-1/3">
+            <h2 className="text-blue-800 text-5xl">Level up your notes now!</h2>
+            <p className="mt-8 text-gray-700 text-xl">
+              Drawer.io is a chrome extension with which you can put together
+              your own virtual drawers with all sorts of saved images and
+              texts that interest you.<br></br> 
+              Expand your browser now with our great
+              application create your own drawer and have an organized future.
+            </p>
+            <button
+              className="bg-blue-800 w-36 h-10 text-white rounded mt-4 text-xl"
+              onClick={(e) => handleClick(e, "/reg")}
+            >
+              Sign Up
+            </button>
           </div>
-          <div className="col-sm-8">
-            <div className="fakeimg"><img className="fakeimg" src="/assets/landingpage.svg"/></div>
-          </div>
+          <img
+            className="TopComponent-Cta-Image w-2/3 pl-10 py-10"
+            src="/assets/landingpage.svg"
+          />
         </div>
       </div>
     </>
   );
 }
 
-export default TopComponent
+export default TopComponent;
